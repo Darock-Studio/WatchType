ee14cf959ea542743e5c30794c07d187f3e310fc
Lightning-Lion <zj56789012@outlook.com>
2023-02-06 19:27:25 +0800

    Initial Commit

---
+++ WatchType Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WatchType Watch App
+//
+//  Created by 凌嘉徽 on 2023/2/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchType Watch App/WatchTypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchTypeApp.swift
+//  WatchType Watch App
+//
+//  Created by 凌嘉徽 on 2023/2/6.
+//
+
+import SwiftUI
+
+@main
+struct WatchType_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
